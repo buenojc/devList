ac10f5e9155fd5b47980319f89f324f1f32be2be
Julio Cesar Bueno <buenojc@outlook.com>
2020-05-17 00:42:32 -0300

    Add Repositories to Routes

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Profile from './pages/Profile';
+import Repositories from './pages/Repositories';
 
 class Routes extends Component {
   render() {
@@ -10,6 +11,7 @@ class Routes extends Component {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/profile' component={Profile} />
+          <Route path='/repositories' component={Repositories} />
         </Switch>
       </BrowserRouter>
     );
