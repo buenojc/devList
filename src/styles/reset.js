618a1277c4959947f67d71e3cd08008cad29f915
Julio Cesar Bueno <buenojc@outlook.com>
2020-05-26 01:49:58 -0300

    Add reset link behavior

--- src/styles/reset.js
+++ src/styles/reset.js
@@ -1,4 +1,4 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
 * {
@@ -6,6 +6,12 @@ const GlobalStyles = createGlobalStyle`
   padding: 0;
   box-sizing: border-box;
 }
+
+a{
+  text-decoration:none;
+  border: none;
+}
+
 `;
 
 export default GlobalStyles;
