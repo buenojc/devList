221d1750d88bcbece8273dea6bac867faad50bc2
Julio Cesar Bueno <buenojc@outlook.com>
2020-05-26 01:53:04 -0300

    Create container for navigation

---
+++ src/containers/NavigationWrapper/index.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const NavigationWrapper = styled.div`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 120px;
+  margin: 40px auto 0;
+`;
