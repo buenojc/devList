8677e213c920adbc33966a4c230173e728daffce
Julio Cesar Bueno <buenojc@outlook.com>
2020-05-29 01:18:46 -0300

    Change ordination icon

--- src/components/Dropdown/index.js
+++ src/components/Dropdown/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { FaArrowUp, FaArrowDown } from 'react-icons/fa';
+import { FaArrowUp, FaArrowDown, FaEllipsisV } from 'react-icons/fa';
 import { Wrapper } from './styles';
 
 class Dropdown extends React.Component {
   render() {
     return (
       <Wrapper>
-        <p>Alterar ordenação {<FaArrowDown size={13} />}</p>
+        <p>Alterar ordenação {<FaEllipsisV size={14} />}</p>
         <div>
           <li>Ordenar por Estrelas {<FaArrowUp size={12} />} </li>
           <li>Ordenar por Estrelas {<FaArrowDown size={12} />} </li>
